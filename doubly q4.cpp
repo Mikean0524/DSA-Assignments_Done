3f818afa96b79858af07b8de7e5d5ffa060c08e0
Mikean0524 <agupta28_be24@thapar.edu>
2025-10-31 09:55:46 +0530

    doubly q4.cpp

--- doubly q4.cpp
+++ doubly q4.cpp
@@ -17,21 +17,6 @@ public:
         tail = nullptr;
     }
 
-    void insert(char ch) {
-        node* newnode = new node;
-        newnode->data = ch;
-        newnode->next = nullptr;
-        newnode->prev = nullptr;
-
-        if (head == nullptr) {
-            head = tail = newnode;
-        } else {
-            tail->next = newnode;
-            newnode->prev = tail;
-            tail = newnode;
-        }
-    }
-
     bool isPalindrome() {
         if (head == nullptr)
             return true;
@@ -51,25 +36,4 @@ public:
     }
 };
 
-int main() {
-    doublyLinkedList list;
-    char ch;
-    int n;
-
-    cout << "Enter number of characters: ";
-    cin >> n;
-
-    cout << "Enter characters:\n";
-    for (int i = 0; i < n; i++) {
-        cin >> ch;
-        list.insert(ch);
-    }
-
-    if (list.isPalindrome())
-        cout << "The list is a palindrome.\n";
-    else
-        cout << "The list is not a palindrome.\n";
-
-    return 0;
-}
 
